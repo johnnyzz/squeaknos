81a9ff40a96864d15a1638cc507783c4591132ba
Javier <javier@pocho-laptop.(none)>
2010-04-29 16:10:09 -0300

    Add and rename virtual disk conversion scripts

---
+++ scripts/vmdk-to-raw.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+
+
+if [ "$#" -neq "2" ]; then
+  echo "vmdk to raw. Converts a vmdk disk file to a raw one."
+  echo "Usage: vmdk-to-raw source.vmdk dest.raw"
+fi
+
+qemu-img convert -f vmdk $1 -O raw $2
