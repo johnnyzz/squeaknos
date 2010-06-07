5f1fbed92cfb9da0c87e35a2eb006730bdbff552
Javier <javier@pocho-laptop.(none)>
2010-06-07 17:40:06 -0300

    Add multiboot constants

--- nos/multiboot.h
+++ nos/multiboot.h
@@ -20,6 +20,44 @@
 /* The magic number for the Multiboot header.  */
 #define MULTIBOOT_HEADER_MAGIC		0x1BADB002
 
+
+ /* Flags to be set in the 'flags' member of the multiboot info structure. */
+ 
+ /* is there basic lower/upper memory information? */
+ #define MULTIBOOT_INFO_MEMORY                   0x00000001
+ /* is there a boot device set? */
+ #define MULTIBOOT_INFO_BOOTDEV                  0x00000002
+ /* is the command-line defined? */
+ #define MULTIBOOT_INFO_CMDLINE                  0x00000004
+ /* are there modules to do something with? */
+ #define MULTIBOOT_INFO_MODS                     0x00000008
+ 
+ /* These next two are mutually exclusive */
+ 
+ /* is there a symbol table loaded? */
+ #define MULTIBOOT_INFO_AOUT_SYMS                0x00000010
+ /* is there an ELF section header table? */
+ #define MULTIBOOT_INFO_ELF_SHDR                 0X00000020
+ 
+ /* is there a full memory map? */
+ #define MULTIBOOT_INFO_MEM_MAP                  0x00000040
+ 
+ /* Is there drive info? */
+ #define MULTIBOOT_INFO_DRIVE_INFO               0x00000080
+ 
+ /* Is there a config table? */
+ #define MULTIBOOT_INFO_CONFIG_TABLE             0x00000100
+ 
+ /* Is there a boot loader name? */
+ #define MULTIBOOT_INFO_BOOT_LOADER_NAME         0x00000200
+ 
+ /* Is there a APM table? */
+ #define MULTIBOOT_INFO_APM_TABLE                0x00000400
+ 
+ /* Is there video information? */
+ #define MULTIBOOT_INFO_VIDEO_INFO               0x00000800
+
+
 /* The flags for the Multiboot header.  */
 #ifdef __ELF__
 # define MULTIBOOT_HEADER_FLAGS		0x00000003
