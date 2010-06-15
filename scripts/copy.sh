d622bf050e5d332d3fa4d9043e584b45fa695ea4
Javier <javier@pocho-laptop.(none)>
2010-06-15 17:48:33 -0300

    Modify the image copying script to also copy the sources file.

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -10,8 +10,14 @@ if [ "$#" -ge "2" ]; then
   destFile=$2
 fi
 
+sourcesFile=SqueakV41
+if [ "$#" -ge "3"]; then
+  sourcesFile=$3
+fi
+
 vmware-mount $destFile release/mount
 sudo cp ${imageFile}.image release/mount/SqueakNOS.image
 sudo cp ${imageFile}.changes release/mount/SqueakNOS.changes
 sudo cp boot/iso.template/SqueakNOS.kernel release/mount/SqueakNOS.kernel
+sudo cp ${sourcesFile}.sources release/mount/${sourcesFile}.sources
 vmware-mount -k $destFile
