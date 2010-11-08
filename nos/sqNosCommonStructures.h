31be1309b4fcb03db020bd4d18caf3a9f9981388
Guido Chari <charig@gmail.com>
2010-11-08 18:40:30 -0300

    add image protection saving with paging common structures

---
+++ nos/sqNosCommonStructures.h
@@ -0,0 +1,32 @@
+#ifndef __SQNOS_COMMON_STRUCTURES_H__
+#define __SQNOS_COMMON_STRUCTURES_H__
+
+#include "multiboot.h"
+
+// Video Mode Information
+typedef struct DisplayInfo {
+	int width, height, depth, address, bytesPerScanLine;
+} DisplayInfo;
+
+typedef struct ReadOnlyPage {
+	unsigned long virtualAddress;
+	unsigned long physicalAddress;
+	unsigned char contents[4096];
+} ReadOnlyPage;
+
+typedef struct SnapshotInfo {
+	unsigned long pagesSaved;
+	unsigned long pagesToSave;
+	ReadOnlyPage *pages;
+} SnapshotInfo;
+
+typedef struct Computer {
+	DisplayInfo videoInfo;
+	multiboot_info_t *mbi;
+	void *image;
+	unsigned long snapshotStartAddress, snapshotEndAddress;
+	void (*pageFaultHandler)(unsigned long);
+	SnapshotInfo snapshot;
+} Computer;
+
+#endif  /* __SQNOS_COMMON_STRUCTURES_H__ */
