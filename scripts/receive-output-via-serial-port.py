96cdb47cd1a5e66bf5174b156e028eb034b5746a
Guido Chari <charig@gmail.com>
2011-03-11 15:27:49 -0300

    improve the seial debugger

--- scripts/receive-output-via-serial-port.py
+++ scripts/receive-output-via-serial-port.py
@@ -4,12 +4,13 @@ import socket
 from select import select
 import struct
 import sys
+from time import gmtime, strftime
 
 def chunks(l, n):
     return [l[i:i+n] for i in range(0, len(l), n)]
 
 s1 = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-s1.connect(('./disksbien/serial1'))
+s1.connect(('./vmdk/serial1'))
 
 data = "1"
 while data:
@@ -26,7 +27,11 @@ while data:
 	#pair = splitted_data[0]
 	#print "ord1:", ord(pair[0]), "ord2:", ord(pair[1]) << 4
 	real_data = [ chr(ord(pair[0]) + (ord(pair[1]) << 4)) for pair in splitted_data ]
-			
+	
+
 	sys.stdout.write("".join(real_data))
 
+	if "".join(real_data) == "\n":
+		sys.stdout.write(strftime("%H:%M:%S", gmtime())+" >> ")
 
+        sys.stdout.flush()
